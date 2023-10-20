a01326bd75f1b76a851fd5dc46f2f9a60d0442a5
jasvanthvarma05 <124557360+jasvanthvarma05@users.noreply.github.com>
2023-10-20 23:27:38 +0530

    Add files via upload

---
+++ EXP_10_R.R
@@ -0,0 +1,7 @@
+speed <- c(78.3, 81.8,82, 74.2 ,83.4, 84.5, 82.9, 77.5, 80.9, 70.6 )
+q1 <- quantile(speed,0.25)
+q3 <- quantile(speed,0.75)
+interquartile <- (q3-q1)
+sd <- sd(speed)
+cat(interquartile)
+cat(sd)
